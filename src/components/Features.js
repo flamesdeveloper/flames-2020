f30b40a75ce32b3e3824d1d54ff44209c8495dbe
smidgey <flamesdeveloper@gmail.com>
2020-07-13 19:11:17 +1000

    styling tweaks

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,15 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const FeatureGrid = ({ values }) => (
-  <div className="columns is-multiline is-centered top-section">
+  <div className="columns is-multiline is-centered top-section bottom-section">
     <div className="column is-12">
       <h3 className="has-text-weight-semibold is-size-2 has-text-centered anchor" id="values">
         {values.title}
       </h3>
     </div>
     {values.blurbs.map((item) => (
-      <div key={item.title} className="column is-4">
-        <section className="section-horizontal">
+      <div key={item.title} className="column is-4 is-parent">
+        <section className="section-horizontal is-child box same-height">
           <div className="has-text-centered">
             <h4>{item.title}</h4>
           </div>
