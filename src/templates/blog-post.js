dbf6d36f15ce3b32dc2084456b4cf7cf8ad861db
smidgey <flamesdeveloper@gmail.com>
2020-09-14 23:41:45 +1000

    blogposts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -21,7 +21,7 @@ export const BlogPostTemplate = ({
   return (
     <section className="section">
       {helmet || ''}
-      <div className="container content">
+      <div className="container content" style={{marginTop: '72px'}}>
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
